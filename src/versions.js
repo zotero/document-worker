93edee9590120d7fc2ab504c1b8f6b4e35852314
Martynas Bagdonas <git.martynas@gmail.com>
2026-07-02 12:42:11 +0300

    SDT: Bump PDF processor version

--- src/versions.js
+++ src/versions.js
@@ -7,7 +7,7 @@ export {
 // Zotero compares these exactly to decide whether an existing SDT cache
 // should be regenerated.
 export const SDT_PROCESSOR_VERSIONS = Object.freeze({
-	pdf: 1,
+	pdf: 2,
 	epub: 1,
 	snapshot: 1,
 });
